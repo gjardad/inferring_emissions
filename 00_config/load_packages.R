2f55c32a2a20390c8ec3ed4df3e0e8c97da54794
Jardanovski Gabriel (External) <gabriel.jardanovski@nbb.be>
2026-01-30 04:08:07 +0100

    add load_packages and deleted create_folders

---
+++ 00_config/load_packages.R
@@ -0,0 +1,19 @@
+packages <- c(
+  "dplyr",
+  "tidyr",
+  "stringr",
+  "purrr",
+  "readr",
+  "data.table",
+  "lubridate",
+  "fixest"
+)
+
+invisible(
+  lapply(packages, function(pkg) {
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+      stop("Package not installed: ", pkg)
+    }
+    library(pkg, character.only = TRUE)
+  })
+)
